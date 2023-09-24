ca7b413bbec1ab3beb5662b415aa3926642cff71
khaled <lsnoop75@gmail.com>
2023-09-24 12:03:46 +0100

    apply the patchd dwm-splitstatus.diff and remove the title in dwm

--- config.def.h
+++ config.def.h
@@ -6,6 +6,8 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int splitstatus        = 1;        /* 1 for split status items */
+static const char *splitdelim        = ";";       /* Character used for separating status */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
